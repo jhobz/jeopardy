cb03b1594027e40b1247d3d16529fcc4511e1248
Jeff Hobson <jeffrey296@gmail.com>
2024-06-23 18:26:17 -0400

    Update nameplate styling and split Score into its own component

---
+++ src/components/ScoreDisplay.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import styled from 'styled-components'
+
+type ScoreDisplayProps = {
+    score: number
+}
+
+const ScoreDisplay: React.FC<ScoreDisplayProps> = ({ score }) => {
+    const formattedScore = score.toLocaleString('en-us')
+    return <Score red={score < 0}>{formattedScore}</Score>
+}
+
+const Score = styled.div<{ red?: boolean }>`
+    padding: 0.2em;
+    font-family: var(--board-value-font);
+    font-size: clamp(1rem, 15vw, 16rem);
+    font-weight: 400;
+    text-align: center;
+    background-color: var(--tile-color);
+    color: ${(props) => (props.red ? 'red' : 'white')};
+    text-shadow: calc(1em / 10) calc(1em / 10) calc(1em / 20) #000;
+`
+
+export default ScoreDisplay
--- src/graphics/Nameplate.tsx
+++ src/graphics/Nameplate.tsx
@@ -1,27 +1,43 @@
-import { useReplicant } from '@nodecg/react-hooks';
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { Player } from '../types/schemas';
+import { useReplicant } from '@nodecg/react-hooks'
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import { Player } from '../types/schemas'
+import ScoreDisplay from '../components/ScoreDisplay'
+import styled from 'styled-components'
 
 export const Nameplate: React.FC = () => {
-  const [playersRep] = useReplicant<Player[]>('players')
-  const index = new URLSearchParams(window.location.search).get('index');
+    const [playersRep] = useReplicant<Player[]>('players')
+    const index = new URLSearchParams(window.location.search).get('index')
 
-  if (!index) return <div>No index query parameter specified.</div>;
+    if (!index) return <div>No index query parameter specified.</div>
 
-  const player = playersRep?.find(x => x.id === Number(index));
+    const player = playersRep?.find((x) => x.id === Number(index))
 
-  if (!player) return null;
-  
-  return (
-    <div>
-      <img src={player.nameplateImage} />
-      <div>{player.points}</div>
-    </div>
-  )
+    if (!player) return null
+
+    return (
+        <Container>
+            <ScoreDisplay score={player.points || 0}></ScoreDisplay>
+            <Image src={player.nameplateImage} />
+        </Container>
+    )
 }
+
+const Container = styled.div`
+    --border-width: 2vw;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    background-color: black;
+    gap: var(--border-width);
+    border: var(--border-width) solid black;
+`
+
+const Image = styled.img`
+    min-width: 300px;
+    width: 100%;
+    aspect-ratio: calc(140 / 94);
+`
 
 const root = createRoot(document.getElementById('root')!)
 root.render(<Nameplate />)
-
-
